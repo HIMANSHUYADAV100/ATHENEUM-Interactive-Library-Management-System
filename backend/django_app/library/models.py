ff0bd36c42bb5aca74fcd7d61d965fd64e785f94
HIMANSHUYADAV100 <54714942+HIMANSHUYADAV100@users.noreply.github.com>
2021-04-18 17:48:01 +0530

    physical rack and url added to lib model book
    phy_rack  ( positive integer)
    book_url  ( url VARCHAR 200 by def
    
    default rack address is 1
    default url provided as https://images.unsplash.com/photo-1600431521340-491eca880813?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80

--- backend/django_app/library/models.py
+++ backend/django_app/library/models.py
@@ -30,6 +30,8 @@ class Book(models.Model):
     author = models.ForeignKey(Author, on_delete = models.CASCADE, related_name="books")
     categories = models.ManyToManyField(Category)
     status_b = models.BooleanField(default=True)
+    phy_rack = models.PositiveIntegerField(default=1)
+    book_url = models.URLField(default="https://images.unsplash.com/photo-1600431521340-491eca880813?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80")
 
     def __str__(self):
         return self.title
