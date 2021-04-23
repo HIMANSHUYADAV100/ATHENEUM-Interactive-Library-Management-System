d1c590ad8b26ea005d04571fb267f6e6a41e89a1
P1YU5H-50N1 <me.piyush.360@gmail.com>
2021-04-23 16:11:41 +0530

    alllskdfjl

--- backend/django_app/library/admin.py
+++ backend/django_app/library/admin.py
@@ -5,7 +5,10 @@ from .models import Category
 from .models import Author
 from .models import Book
 from .models import Issued_records
+from .models import LOGofIssued
 
 admin.site.register(Category)
 admin.site.register(Author)
 admin.site.register(Book)
+admin.site.register(Issued_records)
+admin.site.register(LOGofIssued)
--- backend/django_app/mainapp/local_settings.py
+++ backend/django_app/mainapp/local_settings.py
@@ -1,14 +1,14 @@
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'predictiondb',
-        'USER': 'postgres',
-        'PASSWORD': '12345678',
-        'HOST': '127.0.0.1',
-        'PORT': '5451',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'predictiondb',
+#         'USER': 'postgres',
+#         'PASSWORD': '12345678',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5451',
+#     }
+# }
 
 
 ##  (CORS) Cross-Origin Resource Sharing Settings ##
