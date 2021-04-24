f30574b57ed9f31992941bdb7f48dae8fd63276a
P1YU5H-50N1 <me.piyush.360@gmail.com>
2021-04-24 22:48:31 +0530

    Implemented UserAPI to get information about user via serializer

--- backend/django_app/users/views.py
+++ backend/django_app/users/views.py
@@ -1,6 +1,8 @@
 from rest_auth.views import (LoginView, LogoutView, PasswordChangeView)
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
+from .serializers import UserSerializer
+from rest_framework import generics
 
 # Create your views here.
 
@@ -14,3 +16,14 @@ class APILoginView(LoginView):
 
 class APIPasswordUpdateView(PasswordChangeView):
     authentication_classes = [TokenAuthentication]
+    
+    
+    
+class UserAPI(generics.RetrieveAPIView):
+    permission_classes = [
+    IsAuthenticated,
+    ]
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
