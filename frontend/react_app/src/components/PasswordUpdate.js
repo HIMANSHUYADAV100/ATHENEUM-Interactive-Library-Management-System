5ceb44a14312395a15d8d0c98c1ffd3c36de24c1
P1YU5H-50N1 <me.piyush.360@gmail.com>
2021-04-24 22:56:11 +0530

    Removed errors due to invalid props

--- frontend/react_app/src/components/PasswordUpdate.js
+++ frontend/react_app/src/components/PasswordUpdate.js
@@ -1,25 +1,31 @@
-import React from 'react';
-import axios from 'axios';
-import * as settings from '../settings';
+import React from "react";
+import axios from "axios";
+import * as settings from "../settings";
 
-import { makeStyles } from '@material-ui/core/styles';
-import { Avatar, Button, Container, CssBaseline, TextField, Typography } from '@material-ui/core';
-import VpnKeyIcon from '@material-ui/icons/VpnKey';
-
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  Avatar,
+  Button,
+  Container,
+  CssBaseline,
+  TextField,
+  Typography,
+} from "@material-ui/core";
+import VpnKeyIcon from "@material-ui/icons/VpnKey";
 
 const useStyles = makeStyles((theme) => ({
   paper: {
     marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
   },
   avatar: {
     margin: theme.spacing(1),
     backgroundColor: theme.palette.success.main,
   },
   form: {
-    width: '100%', // Fix IE 11 issue.
+    width: "100%", // Fix IE 11 issue.
     marginTop: theme.spacing(1),
   },
   submit: {
@@ -27,7 +33,7 @@ const useStyles = makeStyles((theme) => ({
   },
   success: {
     color: theme.palette.success.main,
-  }
+  },
 }));
 
 function PasswordUpdate(props) {
@@ -39,41 +45,49 @@ function PasswordUpdate(props) {
   const handleFormFieldChange = (event) => {
     setSuccess(false);
     switch (event.target.id) {
-      case 'new_password1': setNewPassword1(event.target.value); break;
-      case 'new_password2': setNewPassword2(event.target.value); break;
-      default: return null;
+      case "new_password1":
+        setNewPassword1(event.target.value);
+        break;
+      case "new_password2":
+        setNewPassword2(event.target.value);
+        break;
+      default:
+        return null;
     }
-
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (new_password1 !== new_password2) {
-      alert("Passwords don't match")
+      alert("Passwords don't match");
     } else {
-      let headers = { 'Authorization': `Token ${props.token}` };
-      let method = 'post';
-      let url = settings.API_SERVER + '/api/auth/update_password/';
+      let headers = { Authorization: `Token ${props.token}` };
+      let method = "post";
+      let url = settings.API_SERVER + "/api/auth/update_password/";
       let passwordFormData = new FormData();
       passwordFormData.append("new_password1", new_password1);
       passwordFormData.append("new_password2", new_password2);
-      let config = { headers, method, url, data: passwordFormData};
+      let config = { headers, method, url, data: passwordFormData };
       //Axios update_password API call
-      axios(config).then(res => {
-        setSuccess(true);
-      }).catch(
-        error => {
-          alert(error)
+      axios(config)
+        .then((res) => {
+          setSuccess(true);
         })
+        .catch((error) => {
+          alert(error);
+        });
     }
-
-  }
+  };
 
   return (
     <Container component="main" maxWidth="xs">
       <CssBaseline />
       <div className={classes.paper}>
-        {success ? <Typography variant="button" className={classes.success} gutterBottom>Password update successful!</Typography> : null}
+        {success ? (
+          <Typography variant="button" className={classes.success} gutterBottom>
+            Password update successful!
+          </Typography>
+        ) : null}
         <Avatar className={classes.avatar}>
           <VpnKeyIcon />
         </Avatar>
@@ -85,31 +99,32 @@ function PasswordUpdate(props) {
             variant="outlined"
             margin="normal"
             required
-            fullWidth
             name="new_password1"
             label="Enter New Password"
             type="password"
             id="new_password1"
             onChange={handleFormFieldChange}
             error={new_password1 !== new_password2}
-            helperText={new_password1 !== new_password2 ? "Passwords don't match" : null}
+            helperText={
+              new_password1 !== new_password2 ? "Passwords don't match" : null
+            }
           />
           <TextField
             variant="outlined"
             margin="normal"
             required
-            fullWidth
             name="new_password2"
             label="Enter Your Password Again"
             type="password"
             id="new_password2"
             onChange={handleFormFieldChange}
             error={new_password1 !== new_password2}
-            helperText={new_password1 !== new_password2 ? "Passwords don't match" : null}
+            helperText={
+              new_password1 !== new_password2 ? "Passwords don't match" : null
+            }
           />
           <Button
             type="submit"
-            fullWidth
             variant="contained"
             color="primary"
             className={classes.submit}
@@ -121,6 +136,5 @@ function PasswordUpdate(props) {
     </Container>
   );
 }
-
 
 export default PasswordUpdate;
