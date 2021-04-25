7b35f1e3badaa860080989217d1512feb6831572
P1YU5H-50N1 <me.piyush.360@gmail.com>
2021-04-25 23:55:03 +0530

    Removed CssBaseLine

--- frontend/react_app/src/components/Login.js
+++ frontend/react_app/src/components/Login.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
-import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
@@ -67,7 +66,6 @@ function Login(props) {
 
   return (
     <div>
-      <CssBaseline />
       <div className={classes.paper}>
         <div style={{ display: "flex", overflow: "hidden", padding: "2vw" }}>
           <div
@@ -153,6 +151,8 @@ function Login(props) {
     </div>
   );
 }
+
+
 
 const mapDispatchToProps = (dispatch) => {
   return {
