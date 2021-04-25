dcf4ea79b97680a49447617bf51253b61691beb9
P1YU5H-50N1 <me.piyush.360@gmail.com>
2021-04-25 19:24:43 +0530

    formatted

--- frontend/react_app/src/App.js
+++ frontend/react_app/src/App.js
@@ -1,29 +1,27 @@
-import React ,{useEffect} from 'react';
-import Urls from './Urls';
-import Layout from './components/Layout';
-import {connect} from 'react-redux';
-import * as actions from './store/authActions';
+import React, { useState, useEffect } from "react";
+import Urls from "./Urls";
+import Layout from "./components/Layout";
+import { connect } from "react-redux";
+import * as actions from "./store/authActions";
 
 const style = {
-  backgroundColor:"#FAE5BD"
-}
+  backgroundColor: "#FAE5BD",
+};
 
 function App(props) {
-
   // Similar to componentDidMount and componentDidUpdate:
   useEffect(() => {
     props.setAuthenticatedIfRequired();
   }, []);
 
-
-  useEffect(()=>{
+  useEffect(() => {
     props.user();
-  })
+  }, []);
 
   return (
-    <div className="App"style = {style} >
+    <div className="App" style={style}>
       <Layout {...props}>
-         <Urls {...props}/>
+        <Urls {...props} />
       </Layout>
     </div>
   );
@@ -32,18 +30,20 @@ function App(props) {
 //This means that one or more of the redux states in the store are available as props
 const mapStateToProps = (state) => {
   return {
-    isAuthenticated: state.auth.token !== null && typeof state.auth.token !== 'undefined',
-    token: state.auth.token
-  }
-}
+    isAuthenticated:
+      state.auth.token !== null && typeof state.auth.token !== "undefined",
+    token: state.auth.token,
+    current_user: state.auth.user,
+  };
+};
 
 //This means that one or more of the redux actions in the form of dispatch(action) combinations are available as props
 const mapDispatchToProps = (dispatch) => {
   return {
     setAuthenticatedIfRequired: () => dispatch(actions.authCheckState()),
     logout: () => dispatch(actions.authLogout()),
-    user:()=>dispatch(actions.loadUser()) 
-  }
-}
+    user: () => dispatch(actions.loadUser()),
+  };
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
