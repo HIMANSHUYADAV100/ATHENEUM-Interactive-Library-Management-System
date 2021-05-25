fbf82241fe5381b466d1662e438599fa5a40c5f1
P1YU5H-50N1 <me.piyush.360@gmail.com>
2021-05-25 13:48:28 +0530

    Files reorganized

--- backend/django_app/prediction/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-import prediction.views as views
-urlpatterns = [
-
-    path('getRecommendations/',
-         views.BookRecommendationAPI.as_view(),
-         name="book_Recommendations")
-]
--- frontend/src/Action/ActionTypes.js
+++
@@ -1,6 +0,0 @@
-export const AUTH_START = 'AUTH_START';
-export const AUTH_SUCCESS = 'AUTH_SUCCESS';
-export const AUTH_FAIL = 'AUTH_FAIL';
-export const AUTH_LOGOUT = 'AUTH_LOGOUT';
-export const USER_LOADING = "USER_LOADING";
-export const USER_LOADED = "USER_LOADED";
